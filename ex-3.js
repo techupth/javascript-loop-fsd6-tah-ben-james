fea2f180748d798d04986645aafd139dc0ef911c
Ben Wattanasup <benwata777@gmail.com>
2024-05-16 15:05:20 +0700

    java script

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,11 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
-
+for (let i = 0; i < scores.length; i++) {
+  if (i === 0) {
+    minScore = scores[i];
+  } else if (scores[i] < minScore) {
+    minScore = scores[i];
+  }
+}
 console.log(minScore);
--- ex-6.js
+++ ex-6.js
@@ -4,4 +4,13 @@ let reversedCompanyName = "";
 
 // Start coding here
 
+//for (let i = companyName.length - 1; i >= 0; i--) {
+//reversedCompanyName += companyName[i];
+//}
+
+//console.log(reversedCompanyName);
+for (let i = 0; i < companyName.length; i++) {
+  reversedCompanyName = companyName[i] + reversedCompanyName;
+}
+
 console.log(reversedCompanyName);
