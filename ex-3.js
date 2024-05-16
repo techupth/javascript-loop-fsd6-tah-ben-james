806a474fec502ac2338b77d3984fd48919830eb5
Your Name <you@example.com>
2024-05-16 15:43:26 +0700

    edit ex1-6

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,14 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
 let minScore;
-// Start coding here
+// Start coding here.
+
+for (let i = 0; i < scores.length; i++) {
+  if (i === 0) {
+    minScore = scores[i];
+  } else if (scores[i] < minScore) {
+    minScore = scores[i];
+  }
+}
 
 console.log(minScore);
